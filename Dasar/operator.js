dfd4c9e70796ce745d835dd7ef141f49230f99ac
AnselZorro <ansellino@gmail.com>
2025-07-03 06:52:09 +0700

    feat: operator

---
+++ Dasar/operator.js
@@ -0,0 +1,54 @@
+let age = 25;
+
+// Unary operator adalah operator yang hanya membutuhkan satu operand
+typeof age; // Mengembalikan tipe data dari variabel age
+
+// Binary operator adalah operator yang membutuhkan dua operand
+5 + 4;
+10 / 2;
+age = 10;
+
+// Ternary operator adalah operator yang membutuhkan tiga operand
+age < 10 ? "You are too young" : "welcome onboard";
+
+// Digunakan untuk menginisiasi nilai
+
+const name = "Dicoding";
+let location = "Bandung";
+
+// digunakan untuk mengubah nilai
+location = "Jakarta";
+
+// Arithmetic Operator
+
+6 + 5; // mengembalikan 11
+7 - 2; // mengembalikan 5
+8 * 5; // mengembalikan 40
+10 / 3; // mengembalikan 3.33
+10 % 2; // mengembalikan 0
+2 * (10 + 2); // mengembalikan 24
+
+// AND
+console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false
+console.log(5 === 5 && 3 < 5); // true
+
+// OR
+console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log(5 === 5 || 3 > 5); // true
+
+// NOT
+console.log(!true); // false
+console.log(!false); // true
+
+// String Operator
+const first = "bekerja";
+const second = "sama";
+const merged = first + second;
+
+console.log(merged); // Output: bekerjasama
