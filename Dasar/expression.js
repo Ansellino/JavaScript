e29316de217670a203e72a24b519153d816effdb
AnselZorro <ansellino@gmail.com>
2025-07-02 16:40:12 +0700

    feat: tambahkan expression.js

--- Dasar/expression.js
+++ Dasar/expression.js
@@ -1,3 +1,6 @@
 const age = 10;
 const name = "Ansel";
-console.log(`Aku ${name}, umurku ${age} tahun.`);
+const uang = 10000.5;
+console.log(
+  `Aku ${name}, umurku ${age} tahun dan aku memiliki uang sebesar Rp${uang}.`
+);
