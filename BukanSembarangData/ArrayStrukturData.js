cae8bb565a18360361b0edde2bcd51a5d6f1c7b6
AnselZorro <ansellino@gmail.com>
2025-07-04 00:39:41 +0700

    feat: array struktur data

---
+++ BukanSembarangData/ArrayStrukturData.js
@@ -0,0 +1,9 @@
+// Menstrukturkan Data dengan Array
+const array = [1, 2, 3];
+console.log(typeof array); // Output: object
+
+// Membuat array menggunakan object constructor
+const users = new Array();
+const numbers = new Array(5);
+console.log(users, numbers); // Output: [ ], [ <5 empty items> ]
+
